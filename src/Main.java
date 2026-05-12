2f8c23170616a46916108e320ba1047083802482
Lavinia <laviniacristina.botea@ulbsibiu.ro>
2026-05-12 11:09:31 +0300

    10

--- src/Main.java
+++ src/Main.java
@@ -183,23 +183,13 @@ public class Main {
         IO.println("                                    ");
         IO.println("                                    ");
 
-        List<Student> students = Arrays.asList(
-                new Student(1025,"Andrei","Popa","ISM141/2", 8.70),
-                new Student(1024,"Ioan","Mihalcea","ISM141/1", 10),
-                new Student(1026,"Anamaria","Prodan","TI131/1", 8.90),
-                new Student(1029,"Bianca","Popescu","TI131/1", 10),
-                new Student(1030,"Maria","Pana","TI131/2", 4.10),
-                new Student(1031,"Gabriela","Mohanu","TI131/2", 7.33),
-                new Student(1032,"Marius","Nasta","TI131/2", 3.20),
-                new Student(1033,"Marius","Nasta","TI131/1", 5.12),
-                new Student(1034,"Andrei","Dobrescu","TI131/2", 2.22)
-        );
+        AplicatieCuStrategy app = new AplicatieCuStrategy();
 
         IStudentiExport strategyConsole = new StudentiInConsola();
 
         Exporter exporter = new Exporter();
 
-        exporter.startExport(strategyConsole, students);
+        exporter.startExport(strategyConsole, app.studenti);
 
 
     }
